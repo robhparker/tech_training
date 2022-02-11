6fcb4bd0b1ce53c39344e20517627eb108b333aa
robhparker <odoo@ampd1.odoo.com>
2022-02-11 15:00:21 +0000

    Add Module

---
+++ odoo_space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_space",
+
+    'summary': """
+        Take it to the Moon!""",
+
+    'description': """
+        Long description of module's purpose
+        To take you and your Tendies to the Moon!
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+       
+        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/space_demo.xml'
+ 
+    ],
+}
